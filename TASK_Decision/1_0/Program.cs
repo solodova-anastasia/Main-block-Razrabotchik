4e3a50e0f46964ec188e7b44428d80ddb26c841b
Anastasia_Sol <an.solodova29@gmail.com>
2023-05-25 13:01:10 +0700

    Console.Write was added

--- TASK_Decision/1_0/Program.cs
+++ TASK_Decision/1_0/Program.cs
@@ -47,4 +47,16 @@ void Print (string[] array)
     {
         Console.Write($"{array[i]}, ");
     }
+    Console.WriteLine();
 }
+
+Console.Write("Введите набор символов через знак (,): ");
+string inStringSimvol = Console.ReadLine();
+string[] arraySimvol = GetArrayStringConsole(inStringSimvol);
+Console.WriteLine();
+Console.Write("Введенные символы: ");
+Print(GetArrayStringConsole(inStringSimvol));
+Console.WriteLine();
+Console.Write("Введенные символы длинной меньше либо равны 3: ");
+Print(GetArrayThreeSimvol(arraySimvol));
+Console.WriteLine();
